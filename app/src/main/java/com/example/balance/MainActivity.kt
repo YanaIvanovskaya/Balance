093fb787973a2325b917d0e57726671fd81f8db9
YanaIvanovskaya <yana.ivanovskaya@bk.ru>
2021-07-07 17:14:07 +0300

    Сделала навигацию между первыми двумя фрагментами

--- app/src/main/java/com/example/balance/MainActivity.kt
+++ app/src/main/java/com/example/balance/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.example.balance
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.main_container,FirstFragment())
-            .commit()
+
     }
+
 }
+
+
+
+
+//    private lateinit var mNavController : NavController
+//
+//        val navHostFragment: NavHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+//        mNavController = navHostFragment.navController
+//
+//
+//        val buttonNext: Button = findViewById(R.id.button_start_next)
+//        buttonNext.setOnClickListener { this.onStartButtonClick() }
+//
+//        val buttonPrevious: Button? = findViewById(R.id.button_previous)
+//        buttonPrevious?.setOnClickListener { this.onStartPreviousButtonClick() }
+//
+//        supportFragmentManager//    private fun onStartButtonClick() {
+//        mNavController.navigate(R.id.secondFragment)
+//    }
+//
+//    private fun onStartPreviousButtonClick() {
+//        mNavController.navigate(R.id.firstFragment)
+//    }
--- app/src/main/java/com/example/balance/SecondFragment.kt
+++ app/src/main/java/com/example/balance/SecondFragment.kt
@@ -8,24 +8,22 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 
-
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.first_fragment,container,false)
+        return inflater.inflate(R.layout.second_fragment,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val buttonStartNext:Button = view.findViewById(R.id.button_start_next)
+        val buttonStartNext: Button = view.findViewById(R.id.button_previous)
         val navController = NavHostFragment.findNavController(this)
 
-        buttonStartNext.setOnClickListener{ navController.navigate(R.id.secondFragment) }
+        buttonStartNext.setOnClickListener{ navController.navigate(R.id.firstFragment) }
 
     }
 
 }
-
